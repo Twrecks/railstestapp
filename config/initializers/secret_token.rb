aa13f633858466648f0d163b78d766f9aefe7c48
default user <tamerelhotiby@gmail.com>
2012-09-13 21:21:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstestapp::Application.config.secret_token = '33688b14416fe35d931fa3209d90d61cabadba4d9696f6c71875edad13d29f0b71d4e0b172b390876dd109f4aa7bcac966238e6fe1d79fa455f70407240d5dce'
